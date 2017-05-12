c72af79adaaa7f127117e6b60860423f417f852c
arakitsky <arakitsky@google.com>
2017-05-12 11:59:41 +0200

    Removed Servlet implementation

--- src/main/java/com/arakitski/google/appengine/contoller/UserController.java
+++ src/main/java/com/arakitski/google/appengine/contoller/UserController.java
@@ -1,13 +1,10 @@
-package com.arakitski.google.contoller;
+package com.arakitski.google.appengine.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by arakitsky on 5/11/17.
- */
 @Controller
 @RequestMapping("/user")
 public class UserController {
